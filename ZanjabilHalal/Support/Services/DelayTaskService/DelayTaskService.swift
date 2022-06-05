5b1264aa1d44a87e0870a92963f8b6c7e592e8d6
Aiub Duduev <aub.duduev.1989@gmail.com>
2022-06-05 20:57:53 +0300

    added DelayTaskService

--- ZanjabilHalal/Support/Services/DelayTaskService/DelayTaskService.swift
+++ ZanjabilHalal/Support/Services/DelayTaskService/DelayTaskService.swift
@@ -9,4 +9,22 @@ import Foundation
 
 final class DelayTaskService {
 	
+	private let queue = OperationQueue()
+	public var execute: ClosureEmpty!
+	
+	public func deleteAfter(seconds: Double) {
+		self.cancelDelete()
+		let date = Date(timeIntervalSinceNow: seconds)
+		self.queue.schedule(after: .init(date)) { [weak self] in
+			guard let self = self else { return }
+			DispatchQueue.main.async { [weak self] in
+				guard let self = self else { return }
+				self.execute()
+			}
+		}
+	}
+	
+	private func cancelDelete() {
+		queue.cancelAllOperations()
+	}
 }
